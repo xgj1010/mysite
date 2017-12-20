4ec177f5e91bf185b0c0fceea27c0c523cb1eb63
xgj1010 <xgj891010>
2017-12-20 11:14:06 +0800

    添加分页

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,5 @@
 from django.core.urlresolvers import reverse
 from django.db import models
-# from django.urls import reverse
 from django.utils import timezone
 from django.contrib.auth.models import User
 
