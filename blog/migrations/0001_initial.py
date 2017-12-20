ec0ba59c81de977d0b1d271e13aaae61de2a4855
xgj1010 <xgj891010>
2017-12-20 11:40:40 +0800

    使用基于类的视图

--- blog/migrations/0001_initial.py
+++ blog/migrations/0001_initial.py
@@ -2,8 +2,8 @@
 from __future__ import unicode_literals
 
 from django.db import migrations, models
-from django.conf import settings
 import django.utils.timezone
+from django.conf import settings
 
 
 class Migration(migrations.Migration):
@@ -16,20 +16,20 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Post',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', primary_key=True, serialize=False, auto_created=True)),
-                ('title', models.CharField(verbose_name='标题', max_length=250)),
-                ('slug', models.SlugField(verbose_name='标签', max_length=250, unique_for_date='publish')),
+                ('id', models.AutoField(verbose_name='ID', auto_created=True, serialize=False, primary_key=True)),
+                ('title', models.CharField(max_length=250, verbose_name='标题')),
+                ('slug', models.SlugField(max_length=250, verbose_name='标签', unique_for_date='publish')),
                 ('body', models.TextField(verbose_name='内容')),
-                ('publish', models.DateTimeField(verbose_name='发布时间', default=django.utils.timezone.now)),
-                ('created', models.DateTimeField(verbose_name='创建时间', auto_now_add=True)),
-                ('updated', models.DateTimeField(verbose_name='更新时间', auto_now_add=True)),
-                ('status', models.CharField(verbose_name='文章状态', max_length=10, default='draft', choices=[('draft', '草稿'), ('published', '已发布的')])),
-                ('author', models.ForeignKey(verbose_name='作者', related_name='blog_posts', on_delete=True, to=settings.AUTH_USER_MODEL)),
+                ('publish', models.DateTimeField(default=django.utils.timezone.now, verbose_name='发布时间')),
+                ('created', models.DateTimeField(auto_now_add=True, verbose_name='创建时间')),
+                ('updated', models.DateTimeField(auto_now_add=True, verbose_name='更新时间')),
+                ('status', models.CharField(choices=[('draft', '草稿'), ('published', '已发布的')], default='draft', max_length=10, verbose_name='文章状态')),
+                ('author', models.ForeignKey(verbose_name='作者', to=settings.AUTH_USER_MODEL, related_name='blog_posts', on_delete=True)),
             ],
             options={
+                'ordering': ('-publish',),
                 'verbose_name': '文章',
                 'verbose_name_plural': '文章',
-                'ordering': ('-publish',),
             },
         ),
     ]
--- blog/urls.py
+++ blog/urls.py
@@ -2,9 +2,12 @@ __author__ = 'xue'
 __date__ = '2017/12/19 22:23'
 from django.conf.urls import url
 
-from .views import post_list, post_detail
+from .views import  post_detail, PostListView
+
+
 
 urlpatterns = [
-    url(r'^$', post_list, name='post_list'),
+    # url(r'^$', post_list, name='post_list'),
+    url(r'^$', PostListView.as_view(), name='post_list'),
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', post_detail, name='post_detail'),
 ]
