319c9715e460ba08f6bed0f99f7aae9f06e8d60d
xgj1010 <xgj891010>
2017-12-20 16:43:41 +0800

    添加RSS订阅

--- blog/urls.py
+++ blog/urls.py
@@ -3,6 +3,7 @@ __date__ = '2017/12/19 22:23'
 from django.conf.urls import url
 
 from .views import post_detail, post_list, post_share
+from .feeds import LatestPostFeed
 
 urlpatterns = [
     url(r'^$', post_list, name='post_list'),
@@ -10,4 +11,5 @@ urlpatterns = [
     url(r'^(?P<year>\d{4})/(?P<month>\d{2})/(?P<day>\d{2})/(?P<post>[-\w]+)/$', post_detail, name='post_detail'),
     url(r'^(?P<post_id>\d+)/share/$', post_share, name='post_share'),
     url(r'^tag/(?P<tag_slug>[-\w]+)/$',post_list,name='post_list_by_tag'),
+    url(r'^feed/$', LatestPostFeed(), name='post_feed'),
 ]
